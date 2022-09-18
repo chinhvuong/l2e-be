ba951499451793c9802b1608a3fc620d7548ca64
Sotatek-ChinhVuong <chinh.vuong@sotatek.com>
2022-09-18 22:08:34 +0700

    nest base

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,26 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {
+    logger: ['log', 'error', 'warn', 'debug', 'verbose'],
+  });
+  // Setup Swagger
+  const config = new DocumentBuilder()
+    .addBearerAuth()
+    .setTitle('L2E API')
+    .setDescription('The L2E API description')
+    .setVersion('1.0')
+    .addTag('L2E')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, document);
+  // Global Validation Custom
+  app.useGlobalPipes(new ValidationPipe({ transform: true, whitelist: true }));
+
+  app.enableCors();
   await app.listen(3000);
 }
 bootstrap();
