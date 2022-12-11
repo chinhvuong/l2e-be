e7df29cb582a799ba68ac7d549175465dfe8e933
Sotatek-ChinhVuong <vuongvanchinh2001@gmail.com>
2022-12-11 22:59:38 +0700

    test-cicd

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World to L2E BE!';
+    return 'Hello World to L2E BE! Hello I am Chinh';
   }
 }
