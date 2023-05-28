89dc80805f54d5461347869eebdcb368c246ce33
chinhvuong <chinhvuong.dev@gmail.com>
2023-05-28 15:50:18 +0700

    fixbug create certificate

--- src/certificate/certificate.service.ts
+++ src/certificate/certificate.service.ts
@@ -231,7 +231,7 @@ export class CertificateService {
     for (const key in data) {
       content = content.replace(key, data[key]);
     }
-    content.replace(/&/g, '&#38;');
+    content = content.replace(/&/g, '&#38;');
     // console.log(content);
 
     const rs = await this.s3Service.uploadCert(
